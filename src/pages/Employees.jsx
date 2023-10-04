b9db6453aa0c22140c44fb7b4480681f87898787
ranim-taboush <Trendlix@Thinkpad-1>
2023-10-04 17:54:43 +0300

    first commit

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,35 @@
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const Employees = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-4 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => {
+            return <ColumnDirective key={index} {...item} />;
+          })}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,50 @@
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const Orders = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-4 md:p-10 bg-white rounded-3xl">
+      <Header category="الصفحة" title="جدول المراحل الدراسية" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => {
+            return <ColumnDirective key={index} {...item} />;
+          })}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Orders;
